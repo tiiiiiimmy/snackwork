2a028477a7e4871d81e0cd50f05ca583b317f946
tiiiiiimmy <102719366+tiiiiiimmy@users.noreply.github.com>
2026-04-06 23:06:00 +1200

    ✨ feat: Add RegisterRequest DTO

---
+++ src/backend/SnackSpot.Api/Models/DTOs/Auth/RegisterRequest.cs
@@ -0,0 +1,8 @@
+namespace SnackSpot.Api.Models.DTOs.Auth;
+
+public class RegisterRequest
+{
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
